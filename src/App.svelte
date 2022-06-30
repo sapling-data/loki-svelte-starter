70b3377c596512960f5021ffa303ba28cf873763
Andrew Waller <andrew.waller@saplingdata.com>
2022-06-30 15:33:53 -0500

    Update router setup and add default redirect onMount

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,32 @@
 <script>
+  import lokiConfig from '../loki.config';
   import Docs from './lib/Docs.svelte';
   import Navbar from './lib/components/Navbar.svelte';
   import HelloWorld from './lib/HelloWorld.svelte';
-  import { Router, Route } from 'svelte-routing';
-  export let url = '';
+  import { Router, Route, navigate } from 'svelte-routing';
+  import { onMount } from 'svelte';
+  export const navigation = [
+    {
+      name: 'Hello World',
+      to: '/',
+      component: HelloWorld,
+    },
+    {
+      name: 'Docs',
+      to: '/docs',
+      component: Docs,
+    },
+  ]
+  export let basepath = import.meta.env.MODE === 'development'
+    ? `/${lokiConfig.appName}/pages/urn/com/${lokiConfig.cloudName}/${lokiConfig.appModelName}/app/pages/${lokiConfig.pageName}/v`
+    : `/${loki.urn.getLastSegment(loki.app.appInstanceUrn)}/pages/urn/com/${lokiConfig.appRoot}/${lokiConfig.appModelName}/app/pages/${lokiConfig.pageName}/v`;
+
+  if (import.meta.env.MODE === 'development') onMount(() => navigate(basepath + '/', { replace: true }))
 </script>
 
-<main>
-  <Navbar />
-  <Router url={url}>
+<Router>
+  <main>
+    <Navbar navigation={navigation} basepath={basepath} />
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <div class="grid grid-cols-3 mb-20">
         <img
@@ -24,12 +42,9 @@
                 alt="Vite logo"
                 src="https://vitejs.dev/logo.svg">
       </div>
-      <Route path="/">
-        <HelloWorld />
-      </Route>
-      <Route path="docs">
-        <Docs />
-      </Route>
+      {#each navigation as item }
+        <Route path={basepath + item.to} component={item.component} />
+      {/each}
     </div>
-  </Router>
-</main>
+  </main>
+</Router>
